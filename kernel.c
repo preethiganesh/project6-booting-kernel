c43eae0dfc288f525b49741836578017fea8d203
preethi rajagopalan <prajagopalan@smu.edu>
2013-10-30 19:19:52 -0500

    initial

---
+++ kernel.c
@@ -0,0 +1,15 @@
+
+/* A simple Kernel that dispalys a message
+Preethi Rajagopalan 
+
+*/
+int main ()
+{
+	putInMemory (0xB000, 0x8001, 'H');
+	putInMemory (0xB000, 0x8002, 1000);
+			while(1){
+				
+			}
+			
+	return 0;
+}
